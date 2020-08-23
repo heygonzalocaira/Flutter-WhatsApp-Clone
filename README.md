b8076af48b1f3e98fca2f486431a66f16d26d19d
Gonzalo Caira <gc98wootz@gmail.com>
2020-08-23 13:45:03 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Flutter WhatsApp Clone
+
+Replicating whatsapp in flutter
+
+## Still in development
+
+![](https://thehackernews.com/images/-XKMkDNuYNk8/XZWvJvV2JOI/AAAAAAAA1U8/N1xmam_8K0QzV9kS-WJR7y3taPdgR_VlwCLcBGAsYHQ/s728-e100/hacking-whatsapp-account.gif)
+
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Whatsapp Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Whatsapp Clone =)'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'this is the begin',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'Null',
+        child: Icon(Icons.message),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
